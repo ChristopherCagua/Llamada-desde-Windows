ca59b0ca395eeb977d6599aca456fab6c5a1ebaa
ChristopherCagua <caguachristopher@gmail.com>
2021-11-29 22:26:37 -0500

    EjerciciosdeVariosProyectos

---
+++ LlamandoDesdeWin/Llamada.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace LlamandoDesdeWin
+{
+    partial class Llamada
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPrimernumero = new System.Windows.Forms.TextBox();
+            this.txtSegundonumero = new System.Windows.Forms.TextBox();
+            this.txtFactorar = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtSumadelosresultados = new System.Windows.Forms.TextBox();
+            this.txtFactorialdelresultado = new System.Windows.Forms.TextBox();
+            this.btnProcesar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(32, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(243, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingresar el primer numero ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(35, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(254, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingresar el segundo numero ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(35, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(266, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ingresar el numero a factorar";
+            // 
+            // txtPrimernumero
+            // 
+            this.txtPrimernumero.Location = new System.Drawing.Point(318, 30);
+            this.txtPrimernumero.Name = "txtPrimernumero";
+            this.txtPrimernumero.Size = new System.Drawing.Size(100, 20);
+            this.txtPrimernumero.TabIndex = 3;
+            // 
+            // txtSegundonumero
+            // 
+            this.txtSegundonumero.Location = new System.Drawing.Point(318, 80);
+            this.txtSegundonumero.Name = "txtSegundonumero";
+            this.txtSegundonumero.Size = new System.Drawing.Size(100, 20);
+            this.txtSegundonumero.TabIndex = 4;
+            // 
+            // txtFactorar
+            // 
+            this.txtFactorar.Location = new System.Drawing.Point(318, 132);
+            this.txtFactorar.Name = "txtFactorar";
+            this.txtFactorar.Size = new System.Drawing.Size(100, 20);
+            this.txtFactorar.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(561, 216);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 21);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Resultado";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(561, 281);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(173, 21);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "La suma es igual a";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(561, 328);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(202, 21);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "El factorial es igual a";
+            // 
+            // txtSumadelosresultados
+            // 
+            this.txtSumadelosresultados.Location = new System.Drawing.Point(791, 284);
+            this.txtSumadelosresultados.Name = "txtSumadelosresultados";
+            this.txtSumadelosresultados.Size = new System.Drawing.Size(100, 20);
+            this.txtSumadelosresultados.TabIndex = 9;
+            // 
+            // txtFactorialdelresultado
+            // 
+            this.txtFactorialdelresultado.Location = new System.Drawing.Point(791, 331);
+            this.txtFactorialdelresultado.Name = "txtFactorialdelresultado";
+            this.txtFactorialdelresultado.Size = new System.Drawing.Size(100, 20);
+            this.txtFactorialdelresultado.TabIndex = 10;
+            // 
+            // btnProcesar
+            // 
+            this.btnProcesar.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProcesar.Location = new System.Drawing.Point(39, 216);
+            this.btnProcesar.Name = "btnProcesar";
+            this.btnProcesar.Size = new System.Drawing.Size(102, 43);
+            this.btnProcesar.TabIndex = 11;
+            this.btnProcesar.Text = "Procesar";
+            this.btnProcesar.UseVisualStyleBackColor = true;
+            this.btnProcesar.Click += new System.EventHandler(this.btnProcesar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(947, 468);
+            this.Controls.Add(this.btnProcesar);
+            this.Controls.Add(this.txtFactorialdelresultado);
+            this.Controls.Add(this.txtSumadelosresultados);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtFactorar);
+            this.Controls.Add(this.txtSegundonumero);
+            this.Controls.Add(this.txtPrimernumero);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPrimernumero;
+        private System.Windows.Forms.TextBox txtSegundonumero;
+        private System.Windows.Forms.TextBox txtFactorar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtSumadelosresultados;
+        private System.Windows.Forms.TextBox txtFactorialdelresultado;
+        private System.Windows.Forms.Button btnProcesar;
+    }
+}
+
---
+++ LlamandoDesdeWin/Llamada.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LlamandoDesdeWin
+{
+    public partial class Llamada : Form
+    {
+        public Llamada()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProcesar_Click(object sender, EventArgs e)
+        {
+            BibliotecaDeFunciones.Operaciones ob1 = new BibliotecaDeFunciones.Operaciones();
+            double num1 = Convert.ToDouble(txtPrimernumero.Text);
+            double num2 = Convert.ToDouble(txtSegundonumero.Text);
+            int Fac = Convert.ToInt32(txtFactorar.Text);
+            double x1 = ob1.suma(num1, num2);
+            double fac = ob1.factorial(Fac);
+            this.txtSumadelosresultados.Text = x1.ToString();
+            this.txtFactorialdelresultado.Text = fac.ToString();
+        }
+    }
+}
